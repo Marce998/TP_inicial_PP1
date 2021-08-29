db195fd0dea64695c80c8c7fc666802faa9263c7
Tahiel Giordana <tahielgiordana@gmail.com>
2021-08-28 21:24:06 -0300

    Script sql actualizado

--- agenda/sql/scriptAgenda.sql
+++ agenda/sql/scriptAgenda.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE [IF NOT EXISTS] `agenda`;
+CREATE DATABASE IF NOT EXISTS `agenda`;
 USE agenda;
-DROP TABLE [IF EXISTS] `personas`
+DROP TABLE IF EXISTS `personas`
 CREATE TABLE `personas`
 (
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
@@ -13,14 +13,43 @@ CREATE TABLE `personas`
   `Pais` varchar(30) NOT NULL,
   `Provincia` varchar(30) NOT NULL,
   `Localidad` varchar(30) NOT NULL,
+  `ciudadPreferida` longtext NOT NULL,
   PRIMARY KEY (`idPersona`)
 );
+
+DROP TABLE IF EXISTS `tipoContacto`;
 
 CREATE TABLE `tipoContacto`
 (
 `idTipoContacto` int(11) NOT NULL AUTO_INCREMENT,
 `tipo` varchar(45) NOT NULL,
 PRIMARY KEY (`idTipoContacto`)
+);
+
+DROP TABLE IF EXISTS `localidades`;
+DROP TABLE IF EXISTS `provincias`;
+DROP TABLE IF EXISTS `paises`;
+
+CREATE TABLE `paises`(
+	`id_pais` INT(11) AUTO_INCREMENT NOT NULL,
+    `nombre` VARCHAR(45) NOT NULL,
+    primary key(`id_pais`)
+);
+
+CREATE TABLE `provincias`(
+	`id_provincia` INT(11) AUTO_INCREMENT NOT NULL,
+	`nombre` VARCHAR(45) NOT NULL,
+    `id_pais` INT(11) NOT NULL,
+    PRIMARY KEY(`id_provincia`),
+	FOREIGN KEY(`id_pais`) REFERENCES paises (`id_pais`)
+);
+
+CREATE TABLE `localidades`(
+	`id_localidad` int(11) AUTO_INCREMENT NOT NULL,
+    `nombre` VARCHAR (45) NOT NULL,
+    `id_provincia` INT(11),
+	PRIMARY KEY(`id_localidad`),
+    CONSTRAINT FOREIGN KEY(`id_provincia`) REFERENCES provincias (`id_provincia`)
 );
 
 INSERT INTO tipocontacto VALUES(1,"Amigo");
